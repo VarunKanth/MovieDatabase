615adde56d3965b424395438705386529fde3af6
varunkanth <varunkanth1@gmail.com>
2024-07-28 00:30:20 +0530

    Added Section Headers
    Added Section Headers

---
+++ MovieDatabase/Views/SectionHeaderView.swift
@@ -0,0 +1,28 @@
+//
+//  SectionHeaderView.swift
+//  MovieDatabase
+//
+//  Created by Varun Kanth Murugesan on 27/07/24.
+//
+
+import UIKit
+
+class SectionHeaderView: UITableViewHeaderFooterView {
+
+    var sectionHeaderName = "" {
+        didSet{
+            self.headerSectionName.text = sectionHeaderName
+        }
+    }
+    
+    @IBOutlet weak var headerSectionName: UILabel!
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
